8479fe2376c88a38e9ac62d92db89f4ef32a7ddd
Javian Phillips <javianph3@gmail.com>
2025-05-24 02:27:34 -0500

    completed assignment

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int addition = a + b;
+            int subtraction = a - b;
+            int multiplication = a * b;
+
+            int quotient = a / b; 
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+
+            Console.WriteLine("What is the radius of the circle? Input Here");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {AreaOfCircle(radius)}.");
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+        
     }
 }
